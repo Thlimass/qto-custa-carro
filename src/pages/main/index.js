82c799ba92c55d7a3e8fad0255e430193a4a1bea
Thlimass <thlima.silva.souza@gmail.com>
2020-01-26 18:06:07 -0300

    criando o componente de modelo e passando por parametro a marca selecionada

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,6 +2,11 @@ import React, {Component} from "react";
 import api from "../../services/api";
 
 export default class Main extends Component {
+    state = {
+        brands: [],
+        models: [],
+    };
+
     componentDidMount() {
         this.loadBrands();
     }
@@ -9,11 +14,43 @@ export default class Main extends Component {
     loadBrands = async () => {
         const response = await api.get('/brands');
         console.log(response);
+        this.setState({brands: response.data});
     };
 
+
+    loadModels = async (event) => {
+        const parametro = event.target.value;
+        const response = await api.get('/brands/' + parametro + '/models');
+        console.log(response);
+        this.setState({models: response.data});
+    };
+
+    loadYears = async (event) => {
+
+    };
+
+
     render() {
-        return <h1>Quanto custa meu carro?</h1>;
+        return (
+            <div className="formulario-marca">
+
+                {/*Função e componente que seleciona o marca*/}
+                <h3>Selecione a marca</h3>
+                <select onChange={this.loadModels} className="brands-list">{this.state.brands.map(brand => (
+                    <option id={"selecao-marca"} key={brand}>{brand}</option>
+                ))}
+                </select>
+                <br/>
+
+                {/*Função e componente que seleciona o modelo*/}
+                <h3>Selecione a modelo</h3>
+                <select className="models-list">{this.state.models.map(model => (
+                    <option key={model}>{model}</option>
+                ))}
+                </select>
+
+                {/*Função e componente que seleciona o ano*/}
+            </div>
+        )
     }
-
-
 }
